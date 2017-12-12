04925d7cc47f1c920a62c07894ae946dc4fee919
JAnderson11 <jacobanderson1104@gmail.com>
2017-12-12 04:56:01 +0000

    Added defaults

--- models/users_model.js
+++ models/users_model.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 var UserSchema = new Schema({
     username: { type: String, unique: true },
-    char_image: String,
-    char_name: String,
+    char_image: {type: String, default: "https://pre00.deviantart.net/be5f/th/pre/i/2014/296/7/6/_mickey_may_cry___2011__by_mirkomics-d83uqvi.png"},
+    char_name: {type: String, default: "Mickey Mouse"},
     hashed_password: String
 });
 mongoose.model('User', UserSchema);
